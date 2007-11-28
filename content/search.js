35d899314a0639750bf726f8352b9bb7a93db39e
Eliot Horowitz <ehorowitz@shopwiki.com>
2007-11-28 17:14:21 -0500

    search working i think

--- content/search.js
+++ content/search.js
@@ -1,26 +1,80 @@
 // search.js
 
-function index( obj , weights ){
+Search = { 
+    
+    wordRegex : /[,\. ]*\b[,\. ]*/ ,
 
-    var words = Array();
+    fixTable : function( table ){
+        table.ensureIndex( { _index : 1 } );
+    } ,
 
-    for ( var field in weights ){
-        print( "going to weight " + field + " with " + weights[field] );
+    index : function( obj , weights ){
         
-        var s = obj[field];
-        if ( ! s )
-            continue;
+        var words = Array();
         
-        s.split( /[,\. ]*\b[,\. ]*/ ).forEach( function( z ){ 
-                z = z.trim();
-                if ( z.length == 0 ) 
+        for ( var field in weights ){
+
+            var s = obj[field];
+            if ( ! s )
+                continue;
+            
+            s.split( Search.wordRegex ).forEach( function( z ){ 
+                    z = z.trim();
+                    if ( z.length == 0 ) 
+                        return;
+                    z = Stem.stem( z );
+                    if ( ! words.contains( z ) )
+                        words.add( z );
+                } );
+            
+        }
+        
+        obj._searchIndex = words;
+        
+        return obj;
+    } ,
+
+    search : function( table , queryString ){
+        
+        var matchCounts = Object(); // _id -> num
+        var all = Array();
+        var max = 0;
+
+        queryString.split( Search.wordRegex ).forEach( function( z ){
+                
+                z = Stem.stem( z.trim() ).trim();
+                if ( z.length == 0 )
                     return;
-                z = Stem.stem( z );
-                if ( ! words.contains( z ) )
-                    words.add( z );
+                
+                var res = table.find( { _searchIndex : z } , { _id : CrID() , _searchIndex : true }  );
+                
+                while ( res.hasNext() ){
+                    var temp = res.next()._id.toString();
+                    
+                    if ( matchCounts[temp] )
+                        matchCounts[temp]++;
+                    else
+                        matchCounts[temp] = 1;
+
+                    max = Math.max( max , matchCounts[temp] );
+
+                    if ( ! all.contains( temp ) )
+                        all.add( temp );
+                }
             } );
         
+        all.sort( function( l , r ){ 
+                return matchCounts[r] - matchCounts[l];
+            } );
+        
+        var good = Array();
+        all.forEach( function( z ){
+                if ( matchCounts[z] == max ){
+                    good.add( t.find( z ) );
+                    return;
+                }
+            } );
+        
+        return good;
     }
-
-    obj._searchIndex = words;
 }
