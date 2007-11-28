871b5448340caf4e02bbd4d4994f2e67fc731703
Eliot Horowitz <ehorowitz@shopwiki.com>
2007-11-28 00:49:38 -0500

    working on search system

---
+++ content/search.js
@@ -0,0 +1,26 @@
+// search.js
+
+function index( obj , weights ){
+
+    var words = Array();
+
+    for ( var field in weights ){
+        print( "going to weight " + field + " with " + weights[field] );
+        
+        var s = obj[field];
+        if ( ! s )
+            continue;
+        
+        s.split( /[,\. ]*\b[,\. ]*/ ).forEach( function( z ){ 
+                z = z.trim();
+                if ( z.length == 0 ) 
+                    return;
+                z = stemWord( z );
+                if ( ! words.contains( z ) )
+                    words.add( z );
+            } );
+        
+    }
+
+    obj._searchIndex = words;
+}
