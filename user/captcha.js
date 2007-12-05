55fe9e8bd9c6d8f8c5d520187dee7f0f0dd0737f
Eliot Horowitz <ehorowitz@shopwiki.com>
2007-12-05 12:36:30 -0500

    captcha

---
+++ user/captcha.js
@@ -0,0 +1,39 @@
+
+Captcha = { 
+    img : function(){
+
+        if ( ! response )
+            throw( "need a response" );
+
+        if ( ! db )
+            throw( "need a db" );
+
+        var s = "eliot";
+        JSCaptcha.img( s , response );
+        
+        var id = "asd" + Math.random();
+        
+        db._captcha.save( { id : id , s : s , ts : Date() } );
+        request.setCookie( "cid" , id );
+        
+    } ,
+    
+    valid : function(){
+        if ( ! request )
+            return false;
+        
+        var id = request.getCookie( "cid" );
+        if ( ! id )
+            return false;
+        
+        var c = db._captcha.findOne( { id : id } );
+        if ( ! c )
+            return false;
+
+        // check time
+        db._captcha.remove( c );
+        
+        return request.captcha == c.s;
+    }
+    
+};
