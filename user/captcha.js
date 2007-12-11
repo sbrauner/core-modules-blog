3fc8609c19333233c7e7ac3bbc1e8bfd0f1b1162
Eliot Horowitz <ehorowitz@shopwiki.com>
2007-12-11 01:08:03 -0500

    fixed db

--- user/captcha.js
+++ user/captcha.js
@@ -13,24 +13,28 @@ Captcha = {
         
         var id = "asd" + Math.random();
         
-        db._captcha.save( { id : id , s : s , ts : Date() } );
+        db.user.captcha.save( { id : id , s : s , ts : Date() } );
         response.addCookie( "cid" , id );
     } ,
     
     valid : function(){
+        SYSOUT( "in valid" );
         if ( ! request )
             return false;
         
         var id = request.getCookie( "cid" );
+        SYSOUT( "id:" + id );
         if ( ! id )
             return false;
         
-        var c = db._captcha.findOne( { id : id } );
-        if ( ! c )
+        var c = db.user.captcha.findOne( { id : id } );
+        SYSOUT( "c:" + c );
+        if ( ! c ){
             return false;
+        }
 
         // check time
-        db._captcha.remove( c );
+        db.user.captcha.remove( c );
         
         return request.captcha == c.s;
     }
