6ff12a449c09fb81673f2af3ca8093075c5737ce
Dwight <dmerriman@gmail.com>
2007-12-17 17:49:21 -0500

    bug exmaple

---
+++ text/text.js
@@ -0,0 +1,20 @@
+// text.js
+
+Text = {
+
+    snippet: function(s) { 
+	if( !isString(s) )
+	    return s;
+	s = s.substring(0,4000);
+	s = s.replace(/<[^>]*>/g, "");
+	var i = 23;
+	if( s.length > i+2 ) {
+	    while( s.charAt(i) != '.' && s.charAt(i) != ' ' && i < s.length ) //{ 
+		i++;
+		//	    }
+	    s = s.substring(0,i);
+	}
+	return s;
+    }
+
+}
