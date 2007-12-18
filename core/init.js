c41730549e6b88ac402438aefb7c38b08387ab7d
Eliot Horowitz <ehorowitz@shopwiki.com>
2007-12-18 01:34:22 -0500

    cleaning

--- core/init.js
+++ core/init.js
@@ -1,7 +1,7 @@
 
 function mapUrlToJxpFileCore( uri , request ){
     var ua = request.getHeader( "User-Agent" );
-	SYSOUT( "user agent : " + ua );
+
     if ( ua && ua.match( /webdav/i ) )
         return "/~~/webdav.jxp";
     
