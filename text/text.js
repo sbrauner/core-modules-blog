90d259ada29f4b37959b9f8d7a36da23a43f0772
Eliot Horowitz <ehorowitz@shopwiki.com>
2007-12-17 20:15:12 -0500

    stuff

--- text/text.js
+++ text/text.js
@@ -9,9 +9,9 @@ Text = {
 	s = s.replace(/<[^>]*>/g, "");
 	var i = 23;
 	if( s.length > i+2 ) {
-	    while( s.charAt(i) != '.' && s.charAt(i) != ' ' && i < s.length ) //{ 
+	    while( s.charAt(i) != '.' && s.charAt(i) != ' ' && i < s.length ){ 
 		i++;
-		//	    }
+            }
 	    s = s.substring(0,i);
 	}
 	return s;
