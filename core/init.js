915903cb78ca80c634e1c45f36c80e02df99938c
Eliot Horowitz <ehorowitz@shopwiki.com>
2008-01-23 16:19:33 -0500

    msft is nuts

--- core/init.js
+++ core/init.js
@@ -1,8 +1,14 @@
 
 function mapUrlToJxpFileCore( uri , request ){
     var ua = request.getHeader( "User-Agent" );
-
-    if ( ua && ua.match( /webdav/i ) )
+    
+    if ( ua && 
+         ( ua.match( /webdav/i )
+           || ua.match( /Microsoft Data Access Internet Publishing Provider DAV/ )
+           || ua.match( /Microsoft Data Access Internet Publishing Provider Protocol Discovery/ )
+           )
+         ){
         return "/~~/webdav.jxp";
+    }
     
 };
