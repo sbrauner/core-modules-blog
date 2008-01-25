b38788836301c6df897646a97cb1140f154fef88
Eliot Horowitz <ehorowitz@shopwiki.com>
2008-01-25 08:35:32 -0500

    db file

---
+++ db/db.js
@@ -0,0 +1,18 @@
+// db.js
+
+function gt( x ){
+    return { $gt : x };
+}
+
+function lt( x ){
+    return { $lt : x };
+}
+
+
+function ge( x ){
+    return { $ge : x };
+}
+
+function le( x ){
+    return { $le : x };
+}
