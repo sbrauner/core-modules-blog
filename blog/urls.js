e6d5fd1c432fdd1d31988450c19f3e3f8d59b491
Dana Spiegel <dana@10gen.com>
2008-01-31 17:48:07 -0500

    Added a page routing system enabled easy search pages added automatic paging for post lists

--- blog/urls.js
+++ blog/urls.js
@@ -13,114 +13,151 @@ arg ex. { compact: true, limit: 2, filter: {categories: "new_york"} } );
 */
 Blog.handleRequest = function( request , arg ){
     if ( ! arg )
-	arg = {};
+	    arg = {};
 
-    var thePosts = Array();
+    var posts = Array();
     var isPage = false; // page vs. post
+    var pageSize = arg.limit || 15;
+    var isCategorySearch = false;
+    var isDateSearch = false;
+    var pageNumber = 1;
+    var hasMoreResults = false;
+    var search = request.q;
     
-        if ( request.q ){
+    SYSOUT("request.q: " + request.q);
+    SYSOUT("pageSize: " + pageSize);
+    
+    // define a standard search, which restricts pages/posts to entries that are live, and publishDate earlier than now
+    
+    
+    if ( request.q ) {
+        posts = Search.search(db.blog.posts, request.q );
+        posts = posts.filter( function( z ){ return z.live; } );
+	    posts = posts.sort( function( a , b ){ return -1 * a.ts.compareTo( b.ts ); } );
 
-        thePosts = Search.search( posts , request.q );
-
-        thePosts = thePosts.filter( function( z ){ return z.live; } );
-	thePosts = thePosts.sort( function( a , b ){ return -1 * a.ts.compareTo( b.ts ); } );
-
-	var num = 0 ;
-        thePosts = thePosts.filter( function( z ){ return num++ < 20; } );
-    }
-    else {
-        var searchObject = { live : true };
+	    var num = 0 ;
+        posts = posts.filter( function( z ){ return num++ < 20; } );
+    } else {
+        var searchCriteria = { live : true }; // add ts filter
     	var uri = request.getURI();
-	var searchName;
-        var index = uri.match(/^\/index/);
-	if( arg.filter ) { 
-	    searchObject = arg.filter;
-	    searchObject.live = true;
-	}
-	if( !index && uri.match(/^\/[a-zA-Z]/) ) {
-	    if( uri.match(/^\/sa100/) ) {
-	        request.applyServletParams( /sa100\/(.+)\.html/ , [ "name" ] );
-	    	searchName = request.name;
-		if( searchName ) searchName = searchName.replace(/-/g, "_");
+	    var searchName;
+	    var entries;
+
+	    if( arg.filter ) { 
+	        searchCriteria = searchCriteria.concat(arg.filter);
 	    }
-	    else if( uri.match(/..*\/$/) ) { 
-	        request.applyServletParams( /\/(.+)\// , [ "name" ] );
-		searchObject.categories = request.name;
-		searchName = null;
-	    }
-	    else {
-	        request.applyServletParams( /\/(.+)\.html/ , [ "name" ] );
-	    	searchName = request.name;
-	    }
-	    isPage = true;
-	}
-	else {
-	    request.applyServletParams( /(200\d.\d+.+)/ , [ "name" ] );
-	    searchName = request.name;
-	}
+
+        // find any paging instructions in the url
+        page = uri.match(/\/page\/([0-9]*)$/);
+        if (page) {
+            pageNumber = parseInt(page.match(/[0-9]*$/));
+
+            // don't forget to strip out the page from the processed uri
+            uri = uri.replace(page, '');
+        }
+
+        // process the URL
+        // strip out the .html and leading and trailing slash if it exists (for MovableType URL migration)
+        uri = uri.replace(/\.html$/, '').replace(/index$/, '').replace(/\/$/, '').replace(/^\//, '').replace(/-/g, '_');
+
+        SYSOUT("base URI: " + uri);
+        SYSOUT("pageNumber: " + pageNumber);
         
-	if ( uri.match( /.index/ ) )
-	    request.applyServletParams( /index-(\d+)/ , [ "page" ] );
+        // look for a page or post with name = URL, and display it if it exists
+        // TODO: look for a page or post with name = URL replacing '-' with '_', and display it if it exists
+        searchCriteria.name = uri;
+        var entry = db.blog.posts.findOne(searchCriteria);
+        if (entry) {
+            SYSOUT('found a matching ' + entry.cls);
+            search = request.q;
+            return {isPage: true,
+                    posts: [entry],
+                    isCategorySearch: isCategorySearch,
+                    baseSearch: search,
+                    hasMoreResults: hasMoreResults};
+        }
 
-        if ( searchName )
-            searchObject.name = searchName;
-	var pageSize = arg.limit || 15;
-        var cursor = posts.find( searchObject ).sort( { ts : -1 } ).limit( pageSize );
-
-	pageNumber = 1;
-	if ( request.page && parseNumber( request.page ) > 0 ){
-	    pageNumber = parseNumber( request.page );
-	    cursor.skip( pageSize * ( pageNumber - 1 ) );
-	}
-
-        thePosts = cursor.toArray();
-	if( thePosts.length )
-	    isPage = thePosts[0].cls != "entry";
-    }
-
-    if ( searchName && thePosts.length == 0 ){
-        print( "NOT FOUND : " + searchName + "<BR>" );
-    }
-
-
-    if ( searchName && thePosts.length == 1 ){
-
-        if ( request.action == "delete" ){
-	    var numToDelete = parseNumber( request.num );
-            thePosts[0].deleteComment( numToDelete );
-            posts.save( thePosts[0] );
+        // if the URL is empty, display the home page
+        if (uri == '') {
+            searchCriteria = { live : true }; // FIX ME! This should remove the name criteria
+	        if (arg.filter) searchCriteria = searchCriteria.concat(arg.filter);
+            entries = db.blog.posts.find( searchCriteria ).sort( { ts : -1 } ).limit( pageSize + 1 ).skip( pageSize * ( pageNumber - 1 ) );
+        } else {
+            // search categories
+            searchCriteria = { live : true }; // FIX ME! This should remove the name criteria
+	        if (arg.filter) searchCriteria = searchCriteria.concat(arg.filter);
+            searchCriteria.categories = uri;
+            searchName = uri;
+            entries = db.blog.posts.find(searchCriteria).sort( { ts : -1 } ).limit( pageSize  + 1 ).skip( pageSize * ( pageNumber - 1 ) );
+            if (entries.length() > 0) {
+                SYSOUT('found matching entries for category: ' + uri);
+                isCategorySearch = true;
+            } else {
+                // this isn't a category search, so we just assume its a date search or partial url search
+                searchCriteria = { live : true }; // FIX ME! This should remove the name criteria
+    	        if (arg.filter) searchCriteria = searchCriteria.concat(arg.filter);
+                searchCriteria.name = new RegExp('^' + uri.replace(/\//, '\\/'));
+                SYSOUT(searchCriteria.name.toString())
+                entries = db.blog.posts.find(searchCriteria).sort( { ts : -1 } ).limit( pageSize  + 1 ).skip( pageSize * ( pageNumber - 1 ) );
+                if (entries) {
+                    SYSOUT('found matching entries for: ' + uri);
+                }
+            }
+            search = uri;
         }
         
-        if ( request.addComment == "yes" ){
-            var newComment = null;
+        posts = entries.toArray();
+        if (entries.length() > pageSize) hasMoreResults = true;
+        if (posts.length > pageSize) {
+            hasMoreResults = true;
+            posts.remove(pageSize);
+        }
+
+        SYSOUT("posts: " + posts.length);
+    }
+
+    if ( searchName && posts.length == 0 ) {
+        print( "NOT FOUND: " + searchName + "<; />" );
+    }   
+
+    if ( searchName && posts.length == 1 ) {
+        if ( request.action == "delete" ) {
+	        var numToDelete = parseNumber( request.num );
+            posts[0].deleteComment( numToDelete );
+            db.blog.posts.save( posts[0] );
+        }
+        
+        if ( request.addComment == "yes" ) {
+            var comment = null;
             
-            if ( user ){
-                newComment = {};
-                newComment.author = user.name;
-                newComment.email = user.email;
-            }
-            else if ( request.yourname && request.yourname.trim().length != 0 &&
-                      request.email && request.email.trim().length != 0 ){
-                if ( Captcha.valid() ){
-                    newComment = {};
-                    newComment.author = request.yourname;
-                    newComment.email = request.email;
-                }
-                else {
+            if ( user ) {
+                comment = {};
+                comment.author = user.name;
+                comment.email = user.email;
+            } else if ( request.yourname && request.yourname.trim().length != 0 && request.email && request.email.trim().length != 0 ) {
+                if ( Captcha.valid() ) {
+                    comment = {};
+                    comment.author = request.yourname;
+                    comment.email = request.email;
+                } else {
                     print( "invalid captcha response : " + request.captcha );
                 }
             }
             
-            newComment.ts = Date();
-            newComment.text = request.txt;
+            comment.ts = Date();
+            comment.text = request.txt;
             
-            if ( newComment ) {
-                thePosts[0].addComment( newComment );
-                posts.save( thePosts[0] );
+            if ( comment ) {
+                posts[0].addComment( comment );
+                db.blog.posts.save( posts[0] );
             }
-
-	}
+	    }
     }
     
-    return { isPage : isPage , posts : thePosts };
+    return {isPage: isPage,
+            posts: posts,
+            isCategorySearch: isCategorySearch,
+            baseSearch: search,
+            hasMoreResults: hasMoreResults
+    };
 }
