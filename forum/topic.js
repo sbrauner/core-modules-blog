3278e93edf3b49e2dc39f9ca913a98d600f5bc72
Ethan Glasser-Camp <ethan@Apocalypse.local>
2008-02-01 10:16:48 -0500

    Remove this comment now that I know what it does.

--- forum/topic.js
+++ forum/topic.js
@@ -10,16 +10,3 @@ Forum.Topic = function(){
 Forum.Topic.list = function(){
     db.forum.topics.find({orderby: {pinned: true}}).limit(10);
 };
-
-/*
-db.forum.posts.find( { topic : "xyz" : first : true } );
-
-post = new Post();
-db.forum.posts.save( post );
-
-topic.first = post;
-db.forums.topics.save( topic );
-
-db.forums.topcs.find()[0].first;
-
-*/
