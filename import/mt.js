0978d86044afeebe7a664a4e67233504999510ba
Dana Spiegel <dana@10gen.com>
2008-02-05 16:26:22 -0500

    Updated categories to provide text filtering for html elements updated search processing to handle proper paging

--- import/mt.js
+++ import/mt.js
@@ -69,9 +69,6 @@ while ( res.hasNext() ){
     myPost.excerpt = res.entry_excerpt;
     myPost.mt_id = res.entry_id;
 
-    if ( myPost.comments )
-	myPost.comments = [];
-
     var comments = jdbcDB.query( "SELECT * FROM mt_comment WHERE comment_visible = 1 AND comment_entry_id = " + res.entry_id + " ORDER BY comment_created_on ");
     var commentCount = 0;
     myPost.comments = Object();
