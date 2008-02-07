0c28cc8a76553f4297bb4cdf8ce8098b67d13cec
Eliot Horowitz <ehorowitz@shopwiki.com>
2008-02-07 13:59:00 -0500

    some charts

--- ui/chart/_init.js
+++ ui/chart/_init.js
@@ -1,22 +1,43 @@
 ui.chart = {};
 
-var myHead = [];
+ui.chart.head = [];
 
-myHead.push( "<script src=\"/@@/mochi/1.3.1/lib/MochiKit.js\" type=\"text/javascript\"></script>\n" );
-myHead.push( "<script src=\"/@@/plotkit/0.9.1/excanvas.js\" type=\"text/javascript\"></script>\n" );
-myHead.push( "<script src=\"/@@/plotkit/0.9.1/Base.js\" type=\"text/javascript\"></script>\n" );
-myHead.push( "<script src=\"/@@/plotkit/0.9.1/Layout.js\" type=\"text/javascript\"></script>\n" );
-myHead.push( "<script src=\"/@@/plotkit/0.9.1/Canvas.js\" type=\"text/javascript\"></script>\n" );
-myHead.push( "<script src=\"/@@/plotkit/0.9.1/SweetCanvas.js\" type=\"text/javascript\"></script>\n" );
+ui.chart.head.push( "<script src=\"/@@/mochi/1.3.1/lib/MochiKit.js\" type=\"text/javascript\"></script>\n" );
+ui.chart.head.push( "<script src=\"/@@/plotkit/0.9.1/excanvas.js\" type=\"text/javascript\"></script>\n" );
+ui.chart.head.push( "<script src=\"/@@/plotkit/0.9.1/Base.js\" type=\"text/javascript\"></script>\n" );
+ui.chart.head.push( "<script src=\"/@@/plotkit/0.9.1/Layout.js\" type=\"text/javascript\"></script>\n" );
+ui.chart.head.push( "<script src=\"/@@/plotkit/0.9.1/Canvas.js\" type=\"text/javascript\"></script>\n" );
+ui.chart.head.push( "<script src=\"/@@/plotkit/0.9.1/SweetCanvas.js\" type=\"text/javascript\"></script>\n" );
 
-if ( head ){
-    SYSOUT( "have head :(" );
-    head.concat( myHead );
+
+ui.chart._checkHeadArray = function( a ){
+
+    for ( var i=0; i<ui.chart.head.length; i++ )
+        if ( ! a.contains( ui.chart.head[i] ) )
+            return false;
+
+    return true;
 }
-else if ( globalHead ){
-    SYSOUT( "have global head :)" );
-    globalHead.concat( myHead );
-}
-else {
-    SYSOUT( "have nothing!!!" );
-}
+
+ui.chart.checkHead = function(){
+    
+    if ( globalHead && ui.chart._checkHeadArray( globalHead ) )
+        return;
+
+    if ( head && ui.chart._checkHeadArray( head ) )
+        return;
+    
+    if ( head && ! head.isLocked() ){
+        SYSOUT( "have head :(" );
+        head.concat( ui.chart.head );
+    }
+    else if ( globalHead ){
+        SYSOUT( "have global head :)" );
+        globalHead.concat( ui.chart.head );
+    }
+    else {
+        SYSOUT( "have nothing!!!" );
+    }
+};
+  
+ui.chart.checkHead();
--- ui/chart/datetime.js
+++ ui/chart/datetime.js
@@ -3,16 +3,19 @@
    this has to work client and server side
  */
 
+ui.chart.checkHead();
+
 ui.chart.DateTime = function(){
 
     this._dataName = "data123" ;
-    this._dataCode = " var " + this._dataName + " = [];";
+    this._dataCode = "";// var " + this._dataName + " = [];";
+    this._series = {};
 
     this.height = 500;
     this.width = 700;
-
+    
     this._min = new Date();
-    this._max = new Date();
+    this._max = new Date( 0 );
 };
 
 
@@ -22,8 +25,13 @@ ui.chart.DateTime = function(){
  * @param series (optional) if you have multiple lines
  */
 ui.chart.DateTime.prototype.add = function( date , value , series ){
-    this._dataCode += this._dataName + ".push( [" + date.getTime() + "," + value +  "] );";
+    if ( ! series )
+        series = "DEFAULT";
+    
+    this._series[series] = true;
 
+    this._dataCode += this._dataName + series + ".push( [" + date.getTime() + "," + value +  " ] );";
+    
     if ( date < this._min )
         this._min = date;
     
@@ -33,27 +41,8 @@ ui.chart.DateTime.prototype.add = function( date , value , series ){
 
 ui.chart.DateTime.prototype.print = function( myPrint ){
     
-    var layoutName = "layout" + this._dataName;
+    core.ui.chart.html.datetime( { chart : this , layoutName : "layout" + this._dataName } );
 
-    myPrint( "<div style=\"width:" + this.width + "px; height:" + this.height + "px; \">\n" );
-    myPrint( "<div><canvas id=\"chart" + this._dataName + "\" width=\"" + this.width + "\" height=\"" + this.height + "\"></canvas></div>\n" );
-    
-    myPrint( "<script type=\"text/javascript\">\n" );
-    myPrint( "var " + layoutName + " = new Layout(\"line\");\n" );
-
-    myPrint( this._dataCode );
-    
-    myPrint( layoutName + ".options.xAxis = [ " +  this._min.getTime() + " , " +  this._max.getTime() + " ];\n" );
-    
-    myPrint( layoutName + ".addDataset( \"d1\" , " + this._dataName + " );\n" );
-    
-    myPrint( layoutName + ".xTickLabeler = function(z){ return new Date(z); };\n" );
-    
-    myPrint( layoutName + ".evaluate();\n" );
-
-    myPrint( "var chart = new SweetCanvasRenderer($(\"chart" + this._dataName + "\"), " + layoutName + ");\n" );
-    myPrint( "chart.render();\n" );
-    
-    myPrint( "</script>\n" );
-    myPrint( "</div>\n" );
 };
+
+
