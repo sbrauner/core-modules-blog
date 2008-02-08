5326a392437e7e2fee9a8dc737713bd7ae5f9dea
Ethan Glasser-Camp <ethan@Apocalypse>
2008-02-08 15:58:13 -0500

    Show dropdown for Users.
    * Allow caller of helper.select to pass functions which change the
      value and the label of a given option.
    
    * Explicitly check for matching _ids to see if we should select the
      right object.
    
    * When saving a feature, take the id we're given and look up the right user.

--- app/bugtracker/data/helper.js
+++ app/bugtracker/data/helper.js
@@ -1,16 +1,34 @@
 app.bugtracker.data.helper = {
-    select: function(obj, field, prefix){
+    select: function(obj, field, prefix, opts, view, value){
         s = "";
         s += "<select name=\""+prefix+field+"\">";
         var orig = obj[field];
-        var field_c = field.toUpperCase();
-        var opts = obj[field_c];
+        if(! opts){
+            var field_c = field.toUpperCase();
+            var opts = obj[field_c];
+        }
         for(var f in opts){
-            if(opts[f] == orig)
-                s += "<option selected=1>";
-            else
-                s += "<option>";
-            s += opts[f];
+            var text = opts[f];
+            if(view) text = view(text);
+            var val = opts[f];
+            if(value) val = value(val);
+            s += "<option";
+            // This paranoid check prevents the possibility of opts[f] being
+            // false or null and getting an exception when you access
+            // opts[f]._id.
+            // We use this in bugtracker by passing a list of options for Users,
+            // of which one is a false "No user selected" object.
+            // We need to be able to check if this was the selected user, but we
+            // also need to be able to check if this wasn't.
+            // If opts[f] is some string, then ._id for both objects will be
+            // null, so don't accept that.
+            if((opts[f] == orig) || (opts[f] && opts[f]._id && orig && orig._id && opts[f]._id == orig._id))
+                s += " selected=1"
+
+            if(opts[f] != val)
+                s += " value=\""+ val +"\"";
+            s += ">";
+            s += text;
             s += "</option>";
         }
         s += "</select>";
