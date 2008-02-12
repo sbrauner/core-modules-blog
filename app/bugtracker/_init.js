a8fc6dd2c33614852c48a970a492e2d437c43a2c
Ethan Glasser-Camp <ethan@Apocalypse>
2008-02-12 13:22:44 -0500

    User-enterable products.
    * Use App.config methods to fetch options for product.
    
    * Save unknown products to the DB using add_config.

--- app/bugtracker/_init.js
+++ app/bugtracker/_init.js
@@ -3,20 +3,3 @@ app.bugtracker = Object();
 core.app.bugtracker.bugtracker();
 core.app.bugtracker.data.feature();
 
-app.bugtracker._get_user_list = function (name){
-    alist = app.App.getlist(allowModule, "bugs", name);
-    return alist;
-};
-
-app.bugtracker.list_products = function(){
-    return app.bugtracker._get_user_list("product");
-};
-
-app.bugtracker.list_OSes = function(){
-    return app.bugtracker._get_user_list("OS");
-};
-
-app.bugtracker.list_versions = function(){
-    return app.bugtracker._get_user_list("version");
-};
-
