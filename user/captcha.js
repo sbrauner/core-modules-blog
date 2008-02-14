2a7e656bc9b490853e884ed2a798b8d68acf5aa1
Yellow <yellow@10gen.com>
2008-02-14 02:14:25 +0000

    version to use md5 hashes instead of saving to db not as secure - but no db load

--- user/captcha.js
+++ user/captcha.js
@@ -2,58 +2,80 @@
 Captcha = { 
     
     DEBUG : false ,
-    
+
     img : function(){
+	if ( ! response )
+	    throw( "need a response" );
 
-        if ( ! response )
-            throw( "need a response" );
-
-        if ( ! db )
-            throw( "need a db" );
-
-	if ( Math.random() > .99 ){
-	    var d = new Date();
-	    d = new Date( d.getTime() - ( 1000 * 3600 * 6 ) );
-	    if ( Captcha.DEBUG ) SYSOUT( "deleting before : " + d );
-	    db.user._captcha.remove( { ts : { $lt : d } } );
-	}
-			  
-	
 	var s = "";
 	while ( s.length < 6 )
 	    s += md5( Math.random() ).replace( /\d/g , "" ).substring( 0 , 6 );
-        JSCaptcha.img( s , response );
-        
-	var obj = { s : s , ts : Date() };
-        db.user._captcha.save( obj );
-	if ( Captcha.DEBUG ) SYSOUT( tojson( obj ) );
-        response.addCookie( "cid" , obj._id );
+	JSCaptcha.img( s , response );
+	
+	return Captcha.USE.img( s );
     } ,
     
     valid : function( request ){
-        if ( Captcha.DEBUG ) SYSOUT( "in valid" );
+	if ( ! request )
+	    return false;
+	
+	var id = request.getCookie( "cid" );
+	if ( Captcha.DEBUG ) SYSOUT( " captcha id : " + id );
+	if ( ! id )
+	    return false;
+	
+	var res = request.captcha;
+	if ( Captcha.DEBUG ) SYSOUT( " captcha res : " + res );
+	if ( ! res )
+	    return false;
 
-        if ( ! request )
-            return false;
-        
-        var id = request.getCookie( "cid" );
+	return Captcha.USE.valid( id , res );
+    } ,
 
-        if ( ! id )
-            return false;
-        
-	id = ObjectId( id );
-	if ( Captcha.DEBUG ) SYSOUT( "id : " + id + " : " + db.user._captcha );
-        var c = db.user._captcha.findOne( id );
-        if ( Captcha.DEBUG ) SYSOUT( "c:" + tojson( c ) );
-        if ( ! c ){
-            return false;
-        }
+    hash : {
+	img : function ( s ){
+	    response.addCookie( "cid" , md5( s ) );
+	} , 
 
-        // TODO: check time
-        //db.user._captcha.remove( c );
-        
-	if ( Captcha.DEBUG ) SYSOUT( "request.captcha : " + request.captcha );
-        return request.captcha == c.s;
+	valid : function( id , res ){
+	    return id == md5( res );
+	}
+    } ,
+    
+    db : { 
+	
+	img : function( s ){
+	    
+	    if ( ! db )
+		throw( "need a db" );
+	    
+	    if ( Math.random() > .99 ){
+		var d = new Date();
+		d = new Date( d.getTime() - ( 1000 * 3600 * 6 ) );
+		if ( Captcha.DEBUG ) SYSOUT( "deleting before : " + d );
+		db.user._captcha.remove( { ts : { $lt : d } } );
+	    }
+	    
+	    var obj = { s : s , ts : Date() };
+	    db.user._captcha.save( obj );
+	    if ( Captcha.DEBUG ) SYSOUT( tojson( obj ) );
+	    response.addCookie( "cid" , obj._id );
+	} ,
+	
+	valid : function( id , res ){
+	    
+	    id = ObjectId( id );
+
+	    var c = db.user._captcha.findOne( id );
+	    if ( Captcha.DEBUG ) SYSOUT( "c:" + tojson( c ) );
+	    
+	    if ( ! c )
+		return false;
+	    	    
+	    return res == c.s;
+	}
     }
     
 };
+
+Captcha.USE = Captcha.hash;
