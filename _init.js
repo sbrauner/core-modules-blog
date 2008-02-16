4e16c537b8867df96726cdcdad6519a9e9d2e609
dwight <yellow@10gen.com>
2008-02-16 22:50:19 +0000

    needs to print, not return

--- _init.js
+++ _init.js
@@ -1,11 +1,15 @@
 
 htmlheader = function(title) {
     title = title || '10gen Application';
-    if (jxp.pieces && jxp.pieces.header) jxp.pieces.header({title: title});
-    else return '<html><head><title>' + title + '</title></head><body>';
+    if ( jxp.pieces && jxp.pieces.header )
+	jxp.pieces.header( {title: title} );
+    else 
+	print( '<html><head><title>' + title + '</title></head><body>' );
 }
 
 htmlfooter = function() {
-    if (jxp.pieces && jxp.pieces.footer) jxp.pieces.footer();
-    else return '</body></html>';
+    if (jxp.pieces && jxp.pieces.footer) 
+	jxp.pieces.footer();
+    else 
+	print('</body></html>');
 }
