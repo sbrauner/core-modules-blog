6e1009a1186c415de69d199b9ba5bc19d75da5a1
dwight <yellow@10gen.com>
2008-02-16 23:16:37 +0000

    override header/footer

--- _init.js
+++ _init.js
@@ -1,14 +1,18 @@
 
 htmlheader = function(title) {
     title = title || '10gen Application';
-    if ( jxp.pieces && jxp.pieces.header )
+    if ( useHeader )
+	useHeader();
+    else if ( jxp.pieces && jxp.pieces.header )
 	jxp.pieces.header( {title: title} );
     else 
 	print( '<html><head><title>' + title + '</title></head><body>' );
 }
 
 htmlfooter = function() {
-    if (jxp.pieces && jxp.pieces.footer) 
+    if ( useFooter )
+	useFooter();
+    else if (jxp.pieces && jxp.pieces.footer) 
 	jxp.pieces.footer();
     else 
 	print('</body></html>');
