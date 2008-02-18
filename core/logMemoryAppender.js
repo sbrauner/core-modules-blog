e1d3b31f5bcba2f678a7d5f4bcb8b2e08652edc4
Eliot Horowitz <ehorowitz@shopwiki.com>
2008-02-18 00:28:47 -0500

    logging framework

---
+++ core/logMemoryAppender.js
@@ -0,0 +1,37 @@
+// logMemoryAppender.js
+
+
+MemoryAppender = {};
+
+MemoryAppender.create = function(){
+    
+    var cache = {};
+    var options = { max : 100 };
+
+    var append = function( loggerName , date , level , msg , throwable , thread ){
+        throw 1;
+        
+        var lst = cache[ loggerName ];
+        if ( ! lst ){
+            lst = new Array( javaCreate( "java.util.LinkedList" ) );
+            cache[ loggerName ] = lst;
+        }
+
+        lst.push( { msg : msg , 
+                    level : level ,  
+                    date : date ,
+                    throwable : throwable , 
+                    thread : thread
+            } );
+        if ( lst.length > options.max  )
+            lst.shift();
+
+    };
+
+    append.cache = cache;
+    append.options = options;
+
+    append.isMemoryAppender = true;
+    
+    return javaCreate( "ed.log.JSAppender" , append );
+};
