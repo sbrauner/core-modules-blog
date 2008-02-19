6288ce1a57dd7449eb915f19d14c3f54d8893f4a
Ethan Glasser-Camp <ethan@Apocalypse>
2008-02-19 17:00:28 -0500

    Polish URI class.
    * Move Util.escape_queryargs into URI, at Dana's suggestion.
    
    * Document a bit.

--- net/uri.js
+++ net/uri.js
@@ -1,4 +1,13 @@
 URI = function(s){
+    // This class is a representation of a URI. Right now it is mostly
+    // intended to support adding/replacement/removal of query args, but it could
+    // presumably be used for other URI-related things too.
+    // Query args are stored in an array, since there can be more than one of the
+    // same arg in a URI.
+    // (If we ever write a multidict or anything, it should be used here.)
+
+    // Parse scheme and hostname. Sometimes this is absent
+    // (the URI starts with a slash).
     if(s.indexOf('://') == -1){
         this.scheme = 'http';
     } else {
@@ -8,12 +17,14 @@ URI = function(s){
     this.hostname = s.substring(0, s.indexOf('/'));
     s = s.substring(s.indexOf('/'), s.length);
 
+    // Parse the path and query args (if any).
     this.args = [];
     if(s.indexOf('?') == -1){
         this.path = s;
     } else {
         this.path = s.substring(0, s.indexOf('?'));
         s = s.substring(s.indexOf('?')+1, s.length);
+        // FIXME: rewrite using split, probably
         var cont = true;
         while(cont){
             if(s.indexOf('&') == -1){
@@ -31,15 +42,17 @@ URI = function(s){
     }
 };
 
-core.util.format();
 URI.prototype.toString = function(){
+    // Generate the string for this URI object.
     if(this.hostname)
         var str = this.scheme + "://" + this.hostname + this.path;
     else
         var str = this.path;
-    var encodeURIComponent = Util.escape_queryargs;
+    var encodeURIComponent = URI.escape_queryargs;
     if(this.args.length > 0){
         str += '?';
+
+        // FIXME: use Array.join.
         for(var i in this.args){
             str += encodeURIComponent(this.args[i].key) + '=' + encodeURIComponent(this.args[i].value) + '&';
         }
@@ -49,10 +62,14 @@ URI.prototype.toString = function(){
 };
 
 URI.prototype.clone = function(){
+    // Clone a URI object. FIXME: when we get Prototype working, use their
+    // Object.clone method.
     return new URI(this.toString());
 };
 
 URI.prototype.addArg = function(key, value){
+    // Add a query arg to this URI.
+    // @return a new URI, with the additional query argument added.
     c = this.clone();
     return c._addArg(key, value);
 };
@@ -63,6 +80,11 @@ URI.prototype._addArg = function(key, value){
 };
 
 URI.prototype.replaceArg = function(key, value){
+    // Replace the first query arg with the key "key" with the value "value".
+    // If there is no query arg with key "key", then just add a key:arg pair at the
+    // end.
+    // @return a new URI, with the old query argument (if any) removed and a new
+    //    one added.
     c = this.clone();
     return c._replaceArg(key, value);
 };
@@ -78,6 +100,9 @@ URI.prototype._replaceArg = function(key, value){
 };
 
 URI.prototype.removeArg = function(key){
+    // Remove the first arg with the key "key" from a URI.
+    // @return a new URI with either the same args (if none had the right key) or
+    //         one less arg (if that arg had the same key).
     c = this.clone();
     c._removeArg(key);
     return c;
@@ -98,3 +123,26 @@ URI.prototype._removeArg = function(key){
     return this;
 };
 
+URI.escape_queryargs = function(s){
+    // This temporary function is meant to be roughly equivalent to the JS
+    // encodeURIComponent function, which isn't implemented yet in the appserver.
+    var re = /[^A-Za-z0-9\._~-]/;
+    var strhex = '0123456789abcdef';
+    var t = '';
+    while(true){
+        var exec = re.exec(s);
+        if(exec == null) break;
+        var i = exec.index;
+        t = t + s.substring(0, i);
+        if(s[i] == ' '){
+            var rep = '+';
+        } else {
+            var n = s.charCodeAt(i);
+            var rep = '%'+strhex.charAt(Math.floor(n/16))+strhex.charAt(n%16);
+        }
+        t = t+rep;
+        s = s.substring(i+1, s.length);
+    }
+    t = t + s;
+    return t;
+};
