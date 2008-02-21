3ed726a56788551589ae9c1af29b942fe828e4d1
Dana Spiegel <dana@10gen.com>
2008-02-20 22:34:21 -0500

    fixed null pointer exception

--- _init.js
+++ _init.js
@@ -1,26 +1,14 @@
 
 htmlheader = function(title) {
     title = title || '10gen Application';
-
-    if ( useHeader )
-	useHeader();
-
-    else if ( jxp.pieces && jxp.pieces.header )
-	jxp.pieces.header( {title: title} );
-
-    else if ( jxp.html.pieces && jxp.html.pieces.header )
-	jxp.html.pieces.header( {title: title} );
-
-    else 
-	print( '<html><head><title>' + title + '</title></head><body>' );
+    if (useHeader) useHeader();
+    else if (jxp.pieces && jxp.pieces.header) jxp.pieces.header(title);
+    else if (jxp.html && jxp.html.pieces && jxp.html.pieces.header) jxp.html.pieces.header(title);
+    else print('<html><head><title>' + title + '</title></head><body>');
 }
 
 htmlfooter = function() {
-    if ( useFooter ) useFooter();
-
-    else if (jxp.html.pieces && jxp.html.pieces.footer) 
-	jxp.html.pieces.footer();
-
-    else 
-	print('</body></html>');
+    if (useFooter) useFooter();
+    else if (jxp.html && jxp.html.pieces && jxp.html.pieces.footer) jxp.html.pieces.footer();
+    else print('</body></html>');
 }
