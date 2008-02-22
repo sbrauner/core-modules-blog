a49098bdc725b2d0c8df67bc318c713ee928b82b
Ethan Glasser-Camp <ethan@Apocalypse>
2008-02-22 17:51:55 -0500

    Support for sub-topics.

--- app/forum/data/topic.js
+++ app/forum/data/topic.js
@@ -3,12 +3,13 @@ Forum.data.Topic = function(){
     this.description = null;
     this.order = null;
     this.latestPost = null;
+    this.parent = null;
     this.postCount = 0;
     this.threadCount = 0;
 };
 
-Forum.data.Topic.list = function(){
-    return db.forum.topics.find().sort({order: 1});
+Forum.data.Topic.list = function(parent){
+    return db.forum.topics.find({parent: parent}).sort({order: 1});
 };
 
 db.forum.forums.setConstructor(Forum.data.Topic);
