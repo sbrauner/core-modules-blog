6f33bd35ac547e56a53b798c2bfa95a93667c589
Ethan Glasser-Camp <ethan@Apocalypse>
2008-02-22 12:40:53 -0500

    Untabify.

--- app/wiki/wiki.js
+++ app/wiki/wiki.js
@@ -6,7 +6,7 @@ app.wiki.WikiController.renamePage = function(wikiPage, newPageName) {
     // ensure we have a page
     if (!wikiPage) return false;
     if (app.wiki.config.readOnly) return false;
-    
+
     // ensure our newPageName is valid
     if (!app.wiki.WikiController.validatePageName(newPageName)) return false;
 
@@ -14,7 +14,7 @@ app.wiki.WikiController.renamePage = function(wikiPage, newPageName) {
     if (db.wiki.findOne( { name: newPageName } )) {
         SYSOUT("Can't rename page to " + newPageName + " since a page with that name already exists.");
         return false;
-    } else { 
+    } else {
         wikiPage.name = newPageName;
         db.wiki.save(wikiPage);
         response.setResponseCode( 302 );
@@ -25,11 +25,11 @@ app.wiki.WikiController.renamePage = function(wikiPage, newPageName) {
 app.wiki.WikiController.validatePageName = function(pageName) {
     // pageName can't be null or empty
     if (!pageName) return false;
-    
+
     // pageName can't be empty
     pageName = pageName.trim();
     if (pageName == '') return false;
-     
+
     // pageName can't be 'Main'
     if (pageName == 'Main') return false;
     else return true;
@@ -45,13 +45,13 @@ app.wiki.WikiController.deletePage = function(wikiPage) {
         SYSOUT("Can't delete page Main");
         return false;
     }
-    
+
     // ensure that the page is resident in the database
     if (!db.wiki.findOne( { name: wikiPage.name } )) {
         SYSOUT("Can't delete page because it hasn't been saved yet: " + wikiPage.name);
         return false
     }
-    
+
     db.wiki.remove(wikiPage);
     response.setResponseCode(302);
      // for ourselves to requery to make sure it is gone, and to get out of "POST" mode
@@ -69,7 +69,7 @@ app.wiki.WikiController.updatePage = function(wikiPage, text) {
 
 app.wiki.WikiController.getCookieCrumb = function(wikiPage) {
     if (!wikiPage) return '';
-    
+
     var cookieCrumb = '';
     var structuredName = wikiPage.getStructuredName();
 
@@ -80,18 +80,18 @@ app.wiki.WikiController.getCookieCrumb = function(wikiPage) {
     for (i = 0; i < structuredName.length; i++) {
         if (i == 0) parentName = structuredName[i];
         else parentName += '.' + structuredName[i];
-        
+
         if (i > 0) cookieCrumb += '.';
         if (i == structuredName.length - 1) cookieCrumb += structuredName[i];
         else cookieCrumb += '<a href="' + parentName + '">' + structuredName[i] + '</a>';
     }
-    
+
     return cookieCrumb;
 }
 
 app.wiki.WikiController.getChildPageNames = function(wikiPage) {
     if (!wikiPage) return [];
-    
+
     var pageNameRegularExpression = /^[^.]*$/;
     if (wikiPage.name != app.wiki.config.prefix + "Main") pageNameRegularExpression = RegExp("^" + wikiPage.name + "\.[^.]+$");
     else if (app.wiki.config.prefix) pageNameRegularExpression = RegExp("^" + app.wiki.config.prefix + "\.[^.]+$");
@@ -101,7 +101,7 @@ app.wiki.WikiController.getChildPageNames = function(wikiPage) {
     childPages.forEach( function(childPage) {
         childPage.name = childPage.name.replace(new RegExp('^' + app.wiki.config.prefix), '');
     });
-    
+
     return childPages;
 
     // remove the prefix from all of the names
