5e40d2f91546267398d31a8219cd482324f5a4ac
Eliot Horowitz <ehorowitz@shopwiki.com>
2008-02-24 09:54:06 -0500

    close button for login/register

--- ui/js/common.js
+++ ui/js/common.js
@@ -1,11 +1,22 @@
 
 String.prototype.trim = function(){
     return this.replace( /(^ *| *$)/ , "" );
-}
+};
+
+function getElement( e ){
+    if ( typeof(e) == "string" )
+        return document.getElementById( e );
+    return e;
+};
 
 function showElement( e ){
-    e = document.getElementById( e );
+    e = getElement( e );
     e.style.display = "block";
+}
+
+function hideElement( e ){
+    e = getElement( e );
+    e.style.display = "none";
 }
 
 function loadDocSync( url ){
