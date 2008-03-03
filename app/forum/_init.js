2ea3b45903cd78b32aa344b233ee1bccb245c7bf
Ethan Glasser-Camp <ethan@Apocalypse>
2008-03-03 13:17:20 -0500

    Code to show deleted posts, undelete posts.

--- app/forum/_init.js
+++ app/forum/_init.js
@@ -2,5 +2,3 @@ app.Forum = {
     renderer: (allowModule && allowModule.forum && allowModule.forum.renderer) || core.app.forum.html,
 };
 
-
-//head.push('<link rel="stylesheet" type="text/css" href="css/forum.css"/>');
