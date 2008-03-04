324b741f61e7e93a0f168c4c44dbdeded75aec88
Yellow <yellow@10gen.com>
2008-03-04 04:18:49 +0000

    fix label stuff

--- blog/category.js
+++ blog/category.js
@@ -7,16 +7,19 @@ Category = function (name, label) {
     this.label = this.label.toLowerCase();
 };
 
-Category.prototype.getLabel = function(label) {
-    category = db.blog.categories.findOne( {name: label} );
-    if (category) 
-        return category.label;
-    
-        return '';
-    
+Category.prototype.getLabel = function(){
+    return this.label || this.name;
+}
+Category.getLabel = function( name ) {
+    var category = db.blog.categories.findOne( {name: name } );
+    if ( category ) 
+        return category.getLabel()
+    return "";
 };
 
 if (db) {
     db.blog.categories.ensureIndex( { mt_id : 1 } );
     db.blog.categories.ensureIndex( { name : 1 } );
+    
+    db.blog.categories.setConstructor( Category );
 }
