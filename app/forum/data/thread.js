24d9baab1093a551d40d0084c05f5dc453b4bfa1
Ethan Glasser-Camp <ethan@Apocalypse.(none)>
2008-03-05 15:00:12 -0500

    Document.

--- app/forum/data/thread.js
+++ app/forum/data/thread.js
@@ -76,6 +76,10 @@ app.Forum.data.Thread.prototype.modifyPostCount = function(num){
     this.save();
 };
 
+// Posts have a deleted field; this field is either false, meaning
+// this post isn't deleted, or it is one of:
+// the string "deleted"
+// the string "moderated"
 app.Forum.data.Thread.prototype.removePost = function(reason, desc_id){
     var p = this.getDescendant(desc_id);
     p.deleted = reason;
