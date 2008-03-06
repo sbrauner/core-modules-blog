edb37e0a5aebb9b5238e0e67706655944a38dcf0
Ethan Glasser-Camp <ethan@Apocalypse>
2008-03-06 10:20:24 -0500

    Fix Search.fixTable to work with recursive object indexing.

--- content/test/test_search.js
+++ content/test/test_search.js
@@ -49,10 +49,29 @@ Search.fixTable(t, OPTIONS);
 assert( Search.search( t, "text", {min: 1}). length == 1);
 assert( Search.search( t, "go", {min: 1}). length == 0);
 
+
+// nested indexing with funny weights
+var OPTIONS = { title: 1, posts: {text: 0.2}};
+o = {
+    title: 'the title',
+    posts: [
+        {text: "some text"},
+        {text: "more text"}
+    ]
+};
+
+Search.index(o, OPTIONS);
+t.save(o);
+Search.fixTable(t, OPTIONS);
+
+assert( Search.search( t, "text", {min: 1}). length == 1);
+assert( Search.search( t, "go", {min: 1}). length == 0);
+
+
 //exit();
 
 
-// Snippets testing
+// ----------------- Snippets testing -----------
 
 s1 = "hi";
 query = "hi";
