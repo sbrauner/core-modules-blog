79033a2917a245993c60f40865847b794278494e
Ethan Glasser-Camp <ethan@Apocalypse>
2008-03-06 12:07:01 -0500

    Create a "set title" method.

--- app/forum/data/thread.js
+++ app/forum/data/thread.js
@@ -35,6 +35,10 @@ app.Forum.data.Thread.prototype.getTitle = function() {
     return this.getFirstPost().title;
 };
 
+app.Forum.data.Thread.prototype.setTitle = function(title){
+    this.getFirstPost().title = title;
+};
+
 app.Forum.data.Thread.prototype.getFirstPost = function(){
     return this.getReplies()[0];
 };
