837b5bda219c63be4aa5be0a6d4fed693ee2ef60
Ethan Glasser-Camp <ethan@Apocalypse.(none)>
2008-03-07 15:07:46 -0500

    URL.unescape_queryargs

--- net/test/test_url.js
+++ net/test/test_url.js
@@ -62,3 +62,4 @@ s = "/bugs/#anchor1";
 u = new URL(s).addArg("number", "40").addArg("truth", "").addArg("you", "honey");
 assert(u.toString() == "/bugs/?number=40&truth=&you=honey#anchor1");
 
+assert(URL.unescape_queryargs('%41+%45') == "A E");
