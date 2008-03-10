553068277b1295521d739c31031c330b5ae56b46
Ethan Glasser-Camp <ethan@Apocalypse>
2008-03-10 11:31:08 -0400

    Some tests for the Window functionality.

--- app/forum/data/test/test_paging.js
+++ app/forum/data/test/test_paging.js
@@ -2,7 +2,9 @@ core.app.forum.data.paging();
 
 var Paging = app.Forum.data.Paging;
 
-var p = new Paging([1, 2, 3, 4, 5], {pageSize: 2, page: 3});
+var data = [1, 2, 3, 4, 5];
+
+var p = new Paging(data, {pageSize: 2, page: 3});
 
 assert(p.numPages() == 3);
 slice = p.slice();
@@ -10,4 +12,13 @@ assert(slice[0] == 5);
 assert(slice.length == 1);
 
 
+var p = new Paging(data, {pageSize: 2, page: 3});
+var w = p.getWindow();
+assert(w.getFirstPage() == 1);
+assert(w.getLastPage() == 3);
+
+var p = new Paging(data, {pageSize: 2, page: 3, padding: 0, minWindow: 0});
+var w = p.getWindow();
+assert(w.getFirstPage() == 3);
+assert(w.getLastPage() == 3);
 
