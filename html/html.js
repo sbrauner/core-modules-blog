7d621ee15e5d12be23daefad6514030fb18a9f23
Kristina Chodorow <kristina@Macintosh.local>
2008-03-13 16:26:23 -0400

    posts can now include newlines

--- html/html.js
+++ html/html.js
@@ -5,7 +5,7 @@
  * Filename: html.js
  * Author: Dana Spiegel (dana@10gen.com)
  */
- 
+
 if (!HTML) HTML = {};
 
 if (!HTML.__init) {
@@ -13,6 +13,16 @@ if (!HTML.__init) {
     HTML.encode = function(s) {
         return s ? s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;') : s;
     };
- 
+
+    HTML.decodeBr = function(s) {
+        s = s.split("&lt;br /&gt;");
+        log("matching: "+s.length);
+        var htmlstr="";
+        for(var i=0; i<s.length; i++) {
+            htmlstr += s[i]+"<br />";
+        }
+        return htmlstr;
+    }
+
     HTML.__init = true;
 }
