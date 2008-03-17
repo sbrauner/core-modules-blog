1790610342315c7e974c26d47cbb137c1862ee26
Ethan Glasser-Camp <ethan@Apocalypse>
2008-03-17 12:33:31 -0400

    Oops, variable rename.

--- app/forum/data/thread.js
+++ app/forum/data/thread.js
@@ -183,4 +183,4 @@ db.forum.threads.ensureIndex({pinned: 1});
 db.forum.threads.ensureIndex({pinned: 1, lastPostTime: 1});
 core.db.db();
 dbutil.associate(app.Forum.data.Thread, db.forum.threads);
-Search.fixTable(db.forum.threads, app.Forum.data.Thread.prototype.SEARCH_OPTIONS);
+Search.fixTable(db.forum.threads, app.Forum.data.Thread.prototype.SEARCH_WEIGHTS);
