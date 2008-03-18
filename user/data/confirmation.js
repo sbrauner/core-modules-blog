2e0eeca66a5f2ca8b77dfd76fdc2ecad97e1a020
Ethan Glasser-Camp <ethan@Apocalypse>
2008-03-18 13:57:31 -0400

    Successfully send an email on registration, redirect correctly, and display note.

--- user/data/confirmation.js
+++ user/data/confirmation.js
@@ -24,7 +24,7 @@ User.Confirmation.prototype.send = function(){
     link.hostname = request.getHost();
     link.port = request.getPort();
     link = link.toString();
-    var body = "Dear "+this.user.nickname+
+    var body = "Dear "+(this.user.nickname || this.user.name)+ "\n" +
         "\n"+
         "Thanks for registering with "+siteName+". Before your user account is activated, you must verify your email address. Please click the link below or copy and past it into your browser.\n"+
         "\n"+
