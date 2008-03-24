0dc1af672831c7b34d502808320311acd5e6919d
Ethan Glasser-Camp <ethan@Apocalypse>
2008-03-24 10:27:53 -0400

    Not very pretty printing. But: JSON.

--- io/encode/_init.js
+++ io/encode/_init.js
@@ -1,2 +1,11 @@
-io.Encode = {};
+io.Encode = {
+    indent: function(num){
+        var result = "";
+        for(var i = 0; i < num; i++){
+            result += " ";
+        }
+        return result;
+    },
+
+};
 
---
+++ io/encode/json.js
@@ -0,0 +1,28 @@
+core.ext.explore();
+core.io.encode.javascript();
+
+io.Encode.JSON = function(obj){
+    return io.Encode.JSON.helper(obj, 0);
+};
+
+io.Encode.JSON.string = function(s) {
+    return '"' + io.Encode.JavaScript.escape(s) + '"'
+};
+
+io.Encode.JSON.helper = function(obj, indent){
+    if(typeof obj == "number"){
+        return new String(obj);
+    }
+    if(typeof obj == "string"){
+        return io.Encode.JSON.string(obj);
+    }
+    if(obj instanceof Array){
+        var str = "[" + obj.map(function(o){ return io.Encode.indent(indent) + io.Encode.JSON.helper(o, indent+2); }).join(',\n' + io.Encode.indent(indent+1)) + "]";
+        return str;
+    }
+    var fieldsary = [];
+    for(var field in obj){
+        fieldsary.push(io.Encode.JSON.string(field) + ": " + io.Encode.JSON.helper(obj[field], indent+2));
+    }
+    return "{" + fieldsary.join(",\n" + io.Encode.indent(indent+1)) + "}";
+};
