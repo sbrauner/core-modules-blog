f4a9d35259e81f6b178b522d23741ce1b2d9b89e
Kristina Chodorow <kristina@Macintosh.local>
2008-04-02 15:01:49 -0400

    Got dependency dropdowns appearing correctly in case_edit

---
+++ app/bugtracker/data/dependency.js
@@ -0,0 +1,8 @@
+app.bugtracker.data.Dependency = function() {};
+
+app.bugtracker.data.Dependency.prototype.TYPE_TITLE = [ '"No dependency"',
+                                                        '"Depends on"',
+						        '"Refers to"',
+                                                        '"Duplicates"',
+                                                        '"Is blocked by"'
+                                                      ];
