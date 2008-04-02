3c90bf0d8356c0ca782d7a9518ecc2541706f260
Ethan Glasser-Camp <ethan@Apocalypse>
2008-04-02 15:48:55 -0400

    Testing auth using the testing.Client thing.

--- user/test/test_auth.js
+++ user/test/test_auth.js
@@ -1,6 +1,8 @@
 db = connect("test");
 db.users.remove({});
 
+core.testing.client();
+
 core.user.user();
 core.user.auth();
 
@@ -10,39 +12,23 @@ u.email = "ethan@10gen.com";
 u.setPassword("truancy");
 db.users.save(u);
 
-var cookies = {};
-
-var getHeaders = function(){
-    var cookieStrings = [];
-    for(var key in cookies){
-        cookieStrings.push(key + "=" + cookies[key]);
-    }
-    return "Cookie:" + cookieStrings.join("; ");
-}
-
-var getRequest = function(query){
-    return javaStatic( "ed.net.httpserver.HttpRequest" , "getDummy" , query , getHeaders());
-};
-
 var prefix = md5(SERVER_HOSTNAME + new Date().roundMinutes(7));
 query = "/?username=Ethan&prefix=" + prefix + "&hash=" + md5(prefix + ":" + u.pass_ha1_name);
 
-
-request = getRequest(query);
-response = { sendRedirectTemporary: function(arg1){ target = arg1; },
-                 addCookie: function(key, val) { cookies[key] = val; }};
-
 log.user.auth.level = log.LEVEL.DEBUG;
 
-var user = Auth.cookie.getUser(request, response, "test");
+client = new testing.Client();
+client.setAnswer("retval");
+var user = client.setURL(query).execute(function(){
+    return Auth.cookie.getUser(request, response, "test");
+});
 
-print(user);
-print(target);
 assert(user.name == "Ethan");
 
-request = getRequest('/');
-print(tojson(cookies));
-print(getHeaders());
-print(request.getCookie('username'));
-var user = Auth.cookie.getUser(request, response, "test");
+var user = client.setURL().execute(function(){
+        return Auth.cookie.getUser(request, response, "test");
+    });
+
 assert(user.name == "Ethan");
+
+db.users.remove({});
