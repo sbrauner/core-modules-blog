c9e46b316c14327c2a7cc4b69ba5f22600522310
Ethan Glasser-Camp <ethan@Apocalypse>
2008-04-03 18:10:28 -0400

    Whitespace.

--- urls.js
+++ urls.js
@@ -41,23 +41,23 @@ Blog.handleRequest = function( request , arg ){
 
     var result = {
 
-        posts : [] , 
+        posts : [] ,
         isPage : false ,
 
         isCategorySearch : false ,
         isDateSearch : false ,
-        baseSearch : "" , 
-        
-        hasPrevious : false , 
-        hasNext : false , 
-        
+        baseSearch : "" ,
+
+        hasPrevious : false ,
+        hasNext : false ,
+
         category : null ,
         search : request.q ,
-        
+
         pageNumber : 1 ,
         pageSize : arg.limit || 30 ,
         searchTerm : request.q ,
-        
+
         uri : arg.uri || request.getURI()
     };
 
@@ -76,21 +76,21 @@ Blog.handleRequest = function( request , arg ){
 
         if (request.q) {
             posts = Search.search(db.blog.posts, request.q , { min : 100 } );
-	    var now = new Date();
+            var now = new Date();
             posts = posts.filter( function( z ){ return z.live && z.ts <= now; } );
             posts = posts.sort( function( a , b ){ return -1 * a.ts.compareTo( b.ts ); } );
-        
+
             var postResults = 0;
             var pageStart = (pageNumber - 1) * pageSize;
             var pageEnd = Math.min(pageNumber * pageSize, posts.length);
-        
+
             Blog.log.debug("posts: " + posts.length);
 
             posts = posts.filter( function( z ) {
                     postResults++
                     return postResults > pageStart && postResults <= pageEnd;
                 });
-        
+
             Blog.log.debug("pageStart: " + pageStart + " pageEnd " + pageEnd );
             Blog.log.debug("postResults: " + postResults);
             Blog.log.debug("page: " + pageNumber);
@@ -99,37 +99,37 @@ Blog.handleRequest = function( request , arg ){
             if ( postResults == 0 ){
                 posts.push( Post.getNoResults() );
                 return result;
-            } 
-        
+            }
+
             hasPrevious = pageStart > 1;
             hasNext = postResults > pageEnd;
             return result;
-        } 
-    
+        }
+
         var searchCriteria = { live : true , ts : { $lt : new Date() } }; // add ts filter
         var entries;
-    
+
         if(arg.filter)
             Blog._addFilters( searchCriteria , arg.filter );
-    
+
         // process the URL
         // strip out the .html and leading and trailing slash if it exists (for MovableType URL migration)
         uri = uri.replace(/\.(jxp|html)$/, '').replace(/index$/, '').replace(/^.rss/ , "/" );
         uri = uri.replace(/\/$/, '').replace(/^\//, '').replace(/-/g, '_').replace( /^(\d\d\d\d)\/0(\d)/ , "$1/$2" );
-    
+
         Blog.log.debug("base URI: [" + uri + "]" );
         Blog.log.debug("pageNumber: " + pageNumber);
-    
+
         // look for a page or post with name = URL, and display it if it exists.
         if ( uri.length != 0 ){
             searchCriteria.name = uri;
             Blog.log.debug( "searchCriteria : " + tojson( searchCriteria ) );
             var entry = db.blog.posts.findOne(searchCriteria);
-	    
-	    if ( ! entry && uri.match( /\/\d\d\d\d\/\d\d?\// ) ){
-		searchCriteria.name = new RegExp( uri.substring( uri.lastIndexOf( "/" ) ) + "$" );
-		entry = db.blog.posts.findOne(searchCriteria);
-	    }
+
+            if ( ! entry && uri.match( /\/\d\d\d\d\/\d\d?\// ) ){
+                searchCriteria.name = new RegExp( uri.substring( uri.lastIndexOf( "/" ) ) + "$" );
+                entry = db.blog.posts.findOne(searchCriteria);
+            }
 
             if (entry) {
                 Blog.log.debug('found a matching ' + entry.cls);
@@ -140,32 +140,32 @@ Blog.handleRequest = function( request , arg ){
                 posts.push( entry );
                 return result;
             }
-            
+
             delete searchCriteria.name;
         }
-    
+
         // if the URL is empty, display the home page
         if (uri == '') {
             searchCriteria.cls = 'entry';
             if ( ! searchCriteria.categories && arg.homeCategory )
                 searchCriteria.categories = arg.homeCategory; // this shouldn't be in the generic blog code, because why would you want to put this kind of limit on the home page by default?
-	    Blog.log.debug( "searchCriteria : " + tojson( searchCriteria ) );
+            Blog.log.debug( "searchCriteria : " + tojson( searchCriteria ) );
             entries = db.blog.posts.find( searchCriteria ).sort( { ts : -1 } ).limit( pageSize + 1 ).skip( pageSize * ( pageNumber - 1 ) );
-        } 
+        }
         else {
             // search categories
             searchCriteria.categories = uri;
             entries = db.blog.posts.find(searchCriteria).sort( { ts : -1 } ).limit( pageSize  + 1 ).skip( pageSize * ( pageNumber - 1 ) );
-        
+
             if (entries.length() > 0) {
                 Blog.log.debug('found matching entries for category: ' + uri);
                 isCategorySearch = true;
                 category = db.blog.categories.findOne({ name: uri });
-            } 
+            }
             else {
                 // this isn't a category search, so we just assume its a date search or partial url search
                 delete searchCriteria.categories;
-            
+
                 searchCriteria.name = new RegExp('^' + uri.replace(/\//g, '\\/'));
                 entries = db.blog.posts.find(searchCriteria).sort( { ts : -1 } ).limit( pageSize  + 1 ).skip( pageSize * ( pageNumber - 1 ) );
                 if (entries.length() > 0) {
@@ -173,21 +173,21 @@ Blog.handleRequest = function( request , arg ){
                 }
             }
         }
-    
+
         search = uri;
-	baseSearch = uri;
-    
+        baseSearch = uri;
+
         posts = entries.toArray();
-	hasPrevious = pageNumber > 1;
+        hasPrevious = pageNumber > 1;
         hasNext = entries.length() > pageSize;
         if (posts.length > pageSize) {
             hasNext = true;
             posts.remove(pageSize);
         }
-    
+
         if (posts.length == 0)
             return Blog.handleMissingUri(uri);
-    
+
     }
     return result;
 };
