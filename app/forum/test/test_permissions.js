84c270489c188c3f0ae224377c9e7d6e77af4823
Ethan Glasser-Camp <ethan@Apocalypse>
2008-04-04 12:28:32 -0400

    A (broken) test case for banning users from the forum.

--- app/forum/test/test_permissions.js
+++ app/forum/test/test_permissions.js
@@ -1,10 +1,10 @@
 db = connect("tests");
+db.forum.banned_ips.remove({});
 
 core.testing.client();
 
 c = new testing.Client();
 c.setAnswer("output");
-c.addHeader("X-Cluster-Client-Ip: 127.0.0.1");
 
 var output = c.execute(function(){
     core.app.forum.index();
@@ -18,3 +18,15 @@ var output = c.withPermission("core.app.forum.admin", function(){
 
 assert(output.match(/onclick="newTopic\(/));
 
+// This test case doesn't currently work; output redirection + throwing
+// exceptions is tricky and I don't have the time right now.
+// FIXME, please!
+
+db.forum.banned_ips.save({ip: "127.0.0.1"});
+
+var output = c.execute(core.app.forum.index);
+
+assert(output.match(/You have been banned!/));
+
+
+db.forum.banned_ips.remove({});
