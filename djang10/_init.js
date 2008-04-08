31ec008d18fd6eafbfa035be35fb7eb7254c68d2
Geir Magnusson Jr <geir@geir-magnusson-jrs-mac-pro.local>
2008-04-08 18:47:42 -0400

    Checkpoint checkin for the django framework POC
    This doesn't work yet - avert your eyes...

---
+++ djang10/_init.js
@@ -0,0 +1,41 @@
+djang10 = {};
+
+
+djang10.invokeSandboxPackage = function(pkg_arr, target, ret) {
+
+    // lock this lock scope as global to prevent leakage upwards
+    
+    scope.setGlobal(true);
+
+    // invoke the required packages
+
+    pkg_arr.forEach(function(z) { z();});
+
+    // invoke the target file
+
+    target();
+
+    // now return the element in scope that's been asked for
+    
+    return scope[ret];
+}
+
+djang10.invokeSandboxFunction = function(pkg_arr, target, func) {
+
+    // lock this lock scope as global to prevent leakage upwards
+
+    scope.setGlobal(true);
+
+    // invoke the required packages
+
+    pkg_arr.forEach(function(z) { z();});
+
+    // invoke the target file
+
+    print("Invokgin " + target);
+    target();
+
+    //now return the result of calling the required function
+    
+    return func();
+}
