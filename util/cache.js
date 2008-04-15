05eb251a6b46fff1ac2772a6b8b04838e0bdebd8
Yellow <yellow@10gen.com>
2008-04-15 19:22:50 +0000

    option for getting old value from cache for post, if has old value, reset it so no one else will block, but keep going

--- util/cache.js
+++ util/cache.js
@@ -11,14 +11,21 @@ TimeOutCache.prototype.add = function( name , value , cacheTime ){
     this.cache[ name ] = { value : value , dead : t };
 };
 
-TimeOutCache.prototype.get = function( name ){
+/**
+* @param oldArr if oldArr exists, and the object is there, but dead
+*               will do oldArr[0] = v;
+*/
+TimeOutCache.prototype.get = function( name , oldArr ){
     var v = this.cache[ name ];
     if ( ! v )
 	return null;
     
     var now = new Date();
-    if ( now.getTime() > v.dead.getTime() )
+    if ( now.getTime() > v.dead.getTime() ){
+	if ( oldArr )
+	    oldArr[0] = v.value;
 	return null;
+    }
     
     return v.value;
 };
