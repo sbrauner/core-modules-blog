0d41fe6a0bb443cf39fedec39c65be8325a5bfbd
kristina <k@wooster.earthlink.net>
2008-04-16 16:36:56 -0400

    added db logger

--- core/logDB.js
+++ core/logDB.js
@@ -5,12 +5,12 @@ core.db.db();
 BasicDBAppender = {};
 
 BasicDBAppender.create = function(){
-    
+
     createCollection( "_logs" , {size:1000000, capped:true} );
 
     var append = function( loggerName , date , level , msg , throwable , thread ){
-        var obj = LogUtil.createObject( loggerName , date , level , msg , throwable , thread );
-        db._logs.save( objg );
+        var obj = LogUtil.createObject( loggerName , date , level.toString() , msg , throwable , thread.toString() );
+        db._logs.save( obj );
     }
 
     append.isBasicDBAppender = true;
@@ -21,13 +21,13 @@ BasicDBAppender.create = function(){
 BasicDBAppender.find = function( logger ){
     if ( ! logger )
         return null;
-    
+
     if ( ! logger.appenders )
         return null;
-    
+
     for ( var i=0; i<logger.appenders.length; i++ ){
         var a = logger.appenders[i];
-        
+
         if ( isObject( a ) && a.isBasicDBAppender )
             return a;
     }
