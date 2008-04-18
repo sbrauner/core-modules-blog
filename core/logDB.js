f32b89887cbab86f4660fdc6dbb46d31014630a5
kristina <k@wooster.earthlink.net>
2008-04-18 15:40:42 -0400

    Added filtering for log viewer

--- core/logDB.js
+++ core/logDB.js
@@ -1,6 +1,7 @@
 // logDB.js
 
 core.db.db();
+core.core.mail();
 
 BasicDBAppender = {};
 
@@ -9,11 +10,21 @@ BasicDBAppender.create = function(){
     createCollection( "_logs" , {size:1000000, capped:true} );
 
     var append = function( loggerName , date , level , msg , throwable , thread ){
-        var obj = LogUtil.createObject( loggerName , date , level.toString() , msg , throwable , thread.toString() );
+        var now = new Date();
+        var lvl = level.toString();
+        msg = msg.toString();
+        if(lvl == "FATAL" && now.getTime() > (this.sendEmail.getTime() + 10*60*1000)) {
+            m = new Mail.Message( "Fatal Log Message", msg );
+            m.addRecipient(  user.email , "to" );
+            m.send( mail );
+            this.sendEmail = now;
+        }
+        var obj = LogUtil.createObject( loggerName , date , lvl, msg, throwable , thread.toString() );
         db._logs.save( obj );
     }
 
     append.isBasicDBAppender = true;
+    append.sentEmail = new Date(0);
 
     return javaCreate( "ed.log.JSAppender" , append );
 };
