44583e02339475b708b53cab92969d4a02c990e0
Ethan <ethan@10gen.com>
2008-04-21 10:21:01 -0400

    Make the active-section li dynamic.

---
+++ admin/data/sectiontree.js
@@ -0,0 +1,25 @@
+return {
+    'system': {
+        'users': {},
+        'database': {
+            'dbprofile': true,
+            'dbview': true,
+        },
+        'files': {
+        },
+        'cron': {
+        },
+        'logs': {
+            'logMemory': true,
+            'logScroll': true
+        },
+    },
+    'editor': {
+    },
+    'git': {
+    },
+    'docs': {
+    },
+    'applications': {
+    }
+};
