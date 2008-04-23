e72140f17feeb3fb934421070aef35ad47f6e861
Ethan <ethan@10gen.com>
2008-04-23 15:20:19 -0400

    Popup window when doing mass diff.

--- admin/assets/gitPopup.js
+++ admin/assets/gitPopup.js
@@ -1,4 +1,21 @@
-var popup = function(_elA){
-    window.open(_elA.href, 'diff', 'width=600,height=640,scrollbars=1');
+var popup = function(href){
+    window.open(href, 'diff', 'width=600,height=640,scrollbars=1');
     return false;
+};
+
+var popupDiffAll = function(){
+    var myChecks = [];
+    var inputs = document.getElementsByTagName("input");
+    var myParams = [];
+    for(var i = 0; i < inputs.length; ++i){
+        if(inputs[i].type == "checkbox" && inputs[i].name == "changed_file"){
+            myChecks.push(inputs[i]);
+        }
+    }
+    for(var i = 0; i < myChecks.length; ++i){
+        if(! myChecks[i].checked) continue;
+        myParams.push("file="+myChecks[i].value);
+    }
+    var href = "gitDiff?"+myParams.join("&");
+    popup(href);
 };
