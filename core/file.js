6c5e1d2172f55c154758c9b4c3bd769eed1e78c2
Yellow <yellow@10gen.com>
2008-05-05 17:03:15 +0000

    docs

--- core/file.js
+++ core/file.js
@@ -1,6 +1,15 @@
+/* local source code filesystem access
+
+   Normally you want to put your files in the database -- see f.jxp
+
+   Note: subject to change, temp implementation.
+*/
 
 File = {};
 
+/* creates a virtual file object, with content.  later you can write it out with 
+   the writeToLocalFile() method.
+*/
 File.create = function( content ){
     return javaCreate( "ed.js.JSInputFile" , null , null , content );
 };
