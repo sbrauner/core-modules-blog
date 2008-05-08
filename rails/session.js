9819b429bbc1e88a9589747ebd7ac624e743a4df
Eliot Horowitz <erh@ERH-OFC-MP.local>
2008-05-08 13:39:16 -0400

    lots of rails stuff

--- rails/session.js
+++ rails/session.js
@@ -6,3 +6,8 @@ CGI.Session.ActiveRecordStore.Session = function(){};
 CGI.Session.ActiveRecordStore.Session.class_eval = function(){
     SYSOUT( "don't know what class_eval shoudld do here" );
 };
+
+
+session = function( options ){
+    SYSOUT( "ignoring session" );
+};
