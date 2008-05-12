4908f6ebd11e12dad8cf0bbef9b01352880cbd8f
Geir Magnusson Jr <geir@geir-magnusson-jrs-mac-pro.local>
2008-05-12 16:07:34 -0400

    blog : add comment

--- _init.js
+++ _init.js
@@ -1,6 +1,8 @@
 Blog = {};
 
+/*
+ *  set up default routing to the blog index page to do standard blog presentation
+ */
 core.core.routes();
 Blog.routes = new Routes();
-
 Blog.routes.setDefault("/~~/blog/index.jxp");
