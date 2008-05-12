7e182321c2a07a584ec443188b23b2db10daa13c
Geir Magnusson Jr <geir@geir-magnusson-jrs-mac-pro.local>
2008-05-12 16:38:56 -0400

    _init.js : add jsdoc documentation for addModule() function

--- _init.js
+++ _init.js
@@ -56,6 +56,9 @@ addToNotice = function(key, value){
 
 /**
  *   Function to mask how we add modules so we can change the underlying mechanism ASAP
+ * 
+ *   @param {String} name - name of module to install.  Currently this is the path from corejs root.  Horrors.
+ *   @param {Object} params - paramter object for the specified module
  */
 addModule = function(name, params) {
 	if (!allowedModules) {
