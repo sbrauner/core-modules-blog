21475a2c0a8e4a1d2b382f6af3cf6fe87f581637
Ethan Glasser-Camp <ethan@Apocalypse.(none)>
2008-05-13 18:12:04 -0400

    Towards a leftNav for all the admin stuff.
    The top-level section that are actually sections need to dynamically
    show/hide their children. Some sections, however, such as git, need to
    actually be links. I'm not sure how these data structures will evolve to
    handle those things.

---
+++ admin/leftNav.js
@@ -0,0 +1,10 @@
+return [
+    {pretty: 'Blog', target: false},
+    {pretty: 'Posts', target: 'posts'},
+    {pretty: 'Drafts', target: 'drafts'},
+    {pretty: 'Pages', target: 'pages'},
+    {pretty: 'Categories', target: 'categories'},
+    {pretty: '404s', target: 'missing'},
+    {pretty: 'Comments', target: 'comments'}
+];
+
