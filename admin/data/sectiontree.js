32028871c2957c8c677e80cf7a4fa116aff9d5a6
Ethan Glasser-Camp <ethan@Apocalypse.(none)>
2008-05-13 17:31:01 -0400

    Flatten toplevel of admin nav.

--- admin/data/sectiontree.js
+++ admin/data/sectiontree.js
@@ -1,44 +1,37 @@
-var applications = {};
-
-if ( allowModule ){
-    for ( var mod in allowModule ){
-        applications[mod] = { $: "/admin/" + mod + "/index" };
-    }
-}
-
-
-return {
-    'system': {
-        'statistics' : {
-            'usage': true,
-        },
-        'users': {},
-        'database': {
-            'dbview': true,
-            'dbprofile': true,
-            'dbshell': true,
-        },
-        'files': {
-        },
-        'cron': {
-        },
-        'logs': {
-            'logMemory': true,
-            'logScroll': true
-        },
-        'shell': {
-        },
+var tree = {
+    'statistics' : {
+        'usage': true,
+    },
+    'users': {},
+    'database': {
+        'dbview': true,
+        'dbprofile': true,
+        'dbshell': true,
+    },
+    'files': {
+    },
+    'cron': {
+    },
+    'logs': {
+        'logMemory': true,
+        'logScroll': true
+    },
+    'shell': {
     },
     'editor': {
         $: '/admin/ed'
     },
     'git': {
         $: '/admin/gitLocal',
-        'git': {}
     },
-    'docs': {
-        'docs': {}
-    },
-    'applications': applications
-
+    'docs': {},
 };
+
+if ( allowModule ){
+    tree['applications'] = false;
+    for ( var mod in allowModule ){
+        tree[mod] = { $: "/admin/" + mod + "/index" };
+    }
+}
+
+return tree;
