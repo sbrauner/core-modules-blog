cb70f203d762d367f4366bbf31148b400e5035c5
Eliot Horowitz <ehorowitz@shopwiki.com>
2008-05-22 23:47:25 -0400

    numbers

--- db/test/test_sql.js
+++ db/test/test_sql.js
@@ -1,27 +1,31 @@
 core.db.sql();
 
+assert( isNumber( SQL._parseToNumber( "5" ) ) );
+
 t = new SQL.Tokenizer( "clicked = 1 " );
 assert( "clicked" == t.nextToken() );
 assert( "=" == t.nextToken() );
-assert( "1" == t.nextToken() );
+assert( 1 == t.nextToken() );
 assert( null == t.nextToken() );
 
 
 t = new SQL.Tokenizer( "clicked=1 " );
 assert( "clicked" == t.nextToken() );
 assert( "=" == t.nextToken() );
-assert( "1" == t.nextToken() );
+assert( 1 == t.nextToken() );
 assert( ! t.hasMore() );
 assert( null == t.nextToken() );
 
 t = new SQL.Tokenizer( "clicked=1 and foo = 5" );
 assert( "clicked" == t.nextToken() );
 assert( "=" == t.nextToken() );
-assert( "1" == t.nextToken() );
+assert( 1 == t.nextToken() );
 assert( "and" == t.nextToken() );
 assert( "foo" == t.nextToken() );
 assert( "=" == t.nextToken() );
-assert( "5" == t.nextToken() );
+var z = t.nextToken();
+assert( 5 == z );
+assert( isNumber( z ) );
 assert( null == t.nextToken() );
 
 f = SQL.parseWhere( "clicked = 1 " );
