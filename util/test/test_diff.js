a23ebba974f344c7862e826d440fc9c48565846c
kristina <k@wooster.earthlink.net>
2008-05-27 08:41:26 -0400

    fixed diff unit test

--- util/test/test_diff.js
+++ util/test/test_diff.js
@@ -20,12 +20,12 @@ var d = Util.Diff.diffInt( a , b );
 var n = Util.Diff.applyBackwardsInt( b , d );
 assert( a == n );
 
-//var d2 = Util.Diff.diff(a, b);
-//var n2 = Util.Diff.applyBackwards(b, d2);
-//assert(d2 == d);
+var d2 = Util.Diff.diff(a, b);
+var n2 = Util.Diff.applyBackwards(b, d2);
+assert(d2 == d);
 
-//assert(n2 == n);
-//assert(n2 == a);
+assert(n2 == n);
+assert(n2 == a);
 
 var a = new Date( 2008, 01, 03, 7, 30, 0, 0 );
 var b = new Date( 2008, 01, 04, 7, 30, 0, 0 );
