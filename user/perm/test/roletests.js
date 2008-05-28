36a4eaa6b08971b402cd2687584866fa05bf7571
Ethan Glasser-Camp <ethan@sundance.nakedbeekey.com>
2008-05-28 18:19:03 -0400

    Split the test cases into two files too.

---
+++ user/perm/test/roletests.js
@@ -0,0 +1,28 @@
+return function(p){
+    var u = {name: "Ethan", email: "ethan@10gen.com", nickname: "Muscles"};
+
+
+    var c = new testing.Client().setAnswer('value');
+
+    var tryAllowed = function(path){
+        return c.setURL(path).execute(function(){ return p.allowed(u, request); });
+    };
+
+    var output = tryAllowed("/woog");
+
+    assert(output == false);
+
+    var output = tryAllowed("/admin/blog/post_edit");
+
+    assert(output == false);
+
+    u.permissions = ["author"];
+
+    var output = tryAllowed("/woog");
+
+    assert(output == false);
+
+    var output = tryAllowed("/admin/blog/post_edit");
+
+    assert(output == true);
+};
--- user/perm/test/test_roledb.js
+++ user/perm/test/test_roledb.js
@@ -1,5 +1,6 @@
 core.testing.client();
 core.user.perm.roledb();
+var cases = core.user.perm.test.roletests();
 db = connect("test");
 
 db.roles.remove({});
@@ -7,29 +8,4 @@ db.roles.save({name: 'author', urls: ['admin/blog/post_edit', 'admin/files', 'ad
 
 var p = new User.Perm.RoleDB(db.roles);
 
-var u = {name: "Ethan", email: "ethan@10gen.com", nickname: "Muscles"};
-
-
-var c = new testing.Client().setAnswer('value');
-
-var tryAllowed = function(path){
-    return c.setURL(path).execute(function(){ return p.allowed(u, request); });
-};
-
-var output = tryAllowed("/woog");
-
-assert(output == false);
-
-var output = tryAllowed("/admin/blog/post_edit");
-
-assert(output == false);
-
-u.permissions = ["author"];
-
-var output = tryAllowed("/woog");
-
-assert(output == false);
-
-var output = tryAllowed("/admin/blog/post_edit");
-
-assert(output == true);
+cases(p);
