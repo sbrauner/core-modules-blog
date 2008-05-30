2f01e9c50d56e26280a0906c7e7f81f33f295c96
Jason Merriman <jason@alleyadmins-macbook-pro.local>
2008-05-30 10:21:11 -0400

    Small text change (Caps issues.)

--- modelCallbackDefault.js
+++ modelCallbackDefault.js
@@ -1,5 +1,5 @@
 
-return { sidebar : [ { title: "Recent posts", posts: blogUtils.getRecentPosts(5) },
+return { sidebar : [ { title: "Recent Posts", posts: blogUtils.getRecentPosts(5) },
                       { title: "Categories", include : "categories" },
                       { title: "Most Popular", posts: Post.getMostPopular(5, 100) },
                       { title: "Most Commented", posts: Post.getMostCommented(5, 100) },
