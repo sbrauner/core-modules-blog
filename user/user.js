8beb8b3bb7411e03958ad71d025da927f69d109a
Geir Magnusson Jr <geir@geir-magnusson-jrs-mac-pro.local>
2008-06-04 16:56:41 -0400

    admin : use the new permission structure set in allowModule, and modify user to access 'permissions' field by method for safety

--- user/user.js
+++ user/user.js
@@ -38,6 +38,16 @@ User.prototype.isAdmin = function(){
     return this.hasPermission( "admin" );
 };
 
+/**
+ *  @return copy of array of permissions. Can be empty.
+ */
+User.prototype.getPermissions = function(){	
+	if (!this.permissions) {
+		return [];
+	}
+	
+	return new Array(this.permissions);
+};
 
 User.prototype.hasPermission = function( perm ){
     if ( ! this.permissions )
