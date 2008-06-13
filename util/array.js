9b3f365b677de9c6534422a697476815d8c3f7d1
Dwight <dmerriman@gmail.com>
2008-06-13 15:10:31 -0400

    minor array comment out method

--- util/array.js
+++ util/array.js
@@ -15,7 +15,8 @@ Array.prototype.distinct = function() {
 Array.prototype.car = function() { return this[0]; }
 Array.prototype.cdr = function() { return this.slice(1); }
 
-/* like PHP implode (combine into a string) */
+/* like PHP implode (combine into a string) 
+   removed: use Array.join() instead.
 Array.prototype.implode = function(sep) { 
     var arr = this;
     if( !arr || arr.length == 0 ) return "";
@@ -26,3 +27,4 @@ Array.prototype.implode = function(sep) {
     }
     return s;
 }
+*/
