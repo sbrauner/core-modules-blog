191a71370bf9612330609a1657e11e2a91fd14da
Igor Bernstein <igor@10gen.com>
2008-06-18 10:02:57 -0400

    removed djang10 templates from corejs

--- templates/djang10.js
+++
@@ -1,7 +0,0 @@
-if(djang10 == null)
-    djang10 = {};
-
-Object.extend(djang10, __djang10.publicApi);
-
-
-return djang10;
--- templates/test/res/customTags.js
+++
@@ -1,9 +0,0 @@
-var djang10 = core.templates.djang10();
-
-register = new djang10.Library();
-
-register.filter("myRev", function(value, param) {
-    log("myRev: [value="+value+"],[param="+param+"]" );
-    return value.reverse() + ":" + param;
-});
-
