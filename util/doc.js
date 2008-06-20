f0d17a75f25c52cf11081143a8e8f117be2afd83
kristina <k@wooster.earthlink.net>
2008-06-20 18:19:06 -0400

    doc can be generated in user-specified dir

--- util/doc.js
+++ util/doc.js
@@ -14,13 +14,13 @@ Util.Doc.toDb = function(file, javaFunc) {
     javaStatic("ed.doc.Generate", javaFunc, file);
 }
 
-Util.Doc.DbToHTML = function() {
+Util.Doc.DbToHTML = function(out_dir) {
     var d = db.doc.find();
     while(d.hasNext()) {
-        this.DbObjToHTML(d.next());
+        this.DbObjToHTML(d.next(), out_dir);
     }
 }
 
-Util.Doc.DbObjToHTML = function(obj) {
-    javaStatic("ed.doc.Generate", "toHTML", tojson(obj._index));
+Util.Doc.DbObjToHTML = function(obj, out_dir) {
+    javaStatic("ed.doc.Generate", "toHTML", tojson(obj._index), out_dir);
 }
