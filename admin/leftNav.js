cc69c2e0ffa281bc59bc51a99fabc9c83ac9e268
Ian White <ian@ian-whites-computer.local>
2008-11-07 11:43:32 -0500

    fixing drafts bug, adding author stats

--- admin/leftNav.js
+++ admin/leftNav.js
@@ -1,12 +1,12 @@
 /**
 *      Copyright (C) 2008 10gen Inc.
-*  
+*
 *    Licensed under the Apache License, Version 2.0 (the "License");
 *    you may not use this file except in compliance with the License.
 *    You may obtain a copy of the License at
-*  
+*
 *       http://www.apache.org/licenses/LICENSE-2.0
-*  
+*
 *    Unless required by applicable law or agreed to in writing, software
 *    distributed under the License is distributed on an "AS IS" BASIS,
 *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,7 +22,8 @@ var tree = [
     {pretty: 'Categories', target: 'categories'},
     {pretty: '404s', target: 'missing'},
     {pretty: 'Comments', target: 'comments'},
-    {pretty: 'Blocked IPs', target: 'blockIP'}
+    {pretty: 'Blocked IPs', target: 'blockIP'},
+  {pretty: 'Author Stats', target: 'stats_authors'}
 ];
 
 var reverse = {
